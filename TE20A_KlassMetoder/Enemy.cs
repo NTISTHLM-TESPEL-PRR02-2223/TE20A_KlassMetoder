46ffb42a3309dfcaa6437683888c2707b8249929
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-06 13:24:21 +0200

    Initial commit

---
+++ TE20A_KlassMetoder/Enemy.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Enemy
+{
+  public int hp = 100;
+
+  public void TakeDamage()
+  {
+    hp -= 2;
+  }
+
+}
---
+++ TE20A_KlassMetoder/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Enemy goomba = new Enemy();
+Enemy bowser = new Enemy();
+
+goomba.TakeDamage();
+bowser.TakeDamage();
+
+
+// Hello("Micke");
+// Hello("Martin");
+
+
+// Console.ReadLine();
+
+
+// static void Hello(string name)
+// {
+//   Console.WriteLine($"HELLO {name}");
+// }
